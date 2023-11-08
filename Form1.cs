b67de441e27765c673e6e236679d25981f02a2d2
NauberGdS <nauber.gutieres@gmail.com>
2023-11-08 19:29:43 -0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerenciadorOVNI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            //Instanciar o outro form/ declarar a classe da outra janela
+            Inicializador janela = new Inicializador();
+            
+            //Mostrar janela:
+            janela.ShowDialog();
+
+            //Fechar o programa todo:
+            Application.Exit();
+        }
+    }
+}
---
+++ Inicializador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerenciadorOVNI
+{
+    public partial class Inicializador : Form
+    {
+        public Inicializador()
+        {
+            InitializeComponent();
+        }
+
+        private void Inicializador_Load(object sender, EventArgs e)
+        {
+            cmbPlaneta.DataSource = BibliotecaOVNI.OVNI.PlanetasValidos;
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            //Obter as informações do form:
+
+            int maxTripulantes = (int)nudTripulantes.Value;
+            int maxAbduzidos = (int)nudAbduzidos.Value;
+            string planetaOrigem = cmbPlaneta.Text;
+
+            //instanciar o OVNI:
+            BibliotecaOVNI.OVNI ovni = 
+                new BibliotecaOVNI.OVNI(maxTripulantes, maxAbduzidos, planetaOrigem);
+
+            //instanciar painel:
+            Painel janela = new Painel(ovni);
+            //abrir painel:
+            janela.ShowDialog();
+        }
+    }
+}
